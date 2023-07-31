a3ddf4ec92c4c69a93df7c7c3e8f471b7f2f3b09
ruixuanzheng <96707192+ruixuanzheng@users.noreply.github.com>
2023-07-31 21:38:05 +0000

    1st

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,4 +2,25 @@ class MiscController < ApplicationController
   def homepage
     render({ :template => "misc_templates/home"})
   end
+
+  def alldirectors
+    render({ :template => "misc_templates/directors"})
+  end
+
+  def allmovies
+    render({ :template => "misc_templates/movies"})
+  end
+
+  def allactors
+    render({ :template => "misc_templates/actors"})
+  end
+
+  def youngest
+    render({ :template => "misc_templates/youngest"})
+  end
+
+  def oldest
+    render({ :template => "misc_templates/eldest"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+  get("/directors", {:controller => "misc", :action => "alldirectors"})
+  get("/movies", {:controller => "misc", :action => "allmovies"})
+  get("/actors", {:controller => "misc", :action => "allactors"})
+  get("/directors/youngest", {:controller => "misc", :action => "youngest"})
+  get("/directors/eldest", {:controller => "misc", :action => "eldest"})
 end
